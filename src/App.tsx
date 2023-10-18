33c5b7b972951f002cdb51dc42ffc10e2d23b46c
Taylor Brown <taymonster@comcast.net>
2023-10-18 16:54:28 -0600

    Config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div >
+        Hello Artera World
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
