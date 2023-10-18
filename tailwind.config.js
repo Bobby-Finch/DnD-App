bbd91f68b5c59aa5377346c998b30bc4ddff185d
Taylor Brown <taymonster@comcast.net>
2023-10-18 17:39:13 -0600

    fixed config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,5 @@ module.exports = {
   content: ["./src//*.{js,jsx,ts,tsx}"],
   mode: "jit",
   theme: {},
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
